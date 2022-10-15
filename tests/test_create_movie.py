4711876e178fb18187f119baa654a0b5fd8029ce
noricha-vr <noricha.vr@gmail.com>
2022-10-15 16:21:58 +0900

    add test params.

--- tests/test_create_movie.py
+++ tests/test_create_movie.py
@@ -4,10 +4,13 @@ from movie_maker import image_to_movie
 import pytest
 
 
-def test_make_movie():
+@pytest.mark.parametrize(('url', 'length'), [
+    ("https://www.google.com", 1),
+    ("https://www.yahoo.com", 50)])
+def test_make_movie(url, length):
     browser = Browser(1280, 720)
-    paths = browser.take_screenshot("https://www.google.com", 100, 5000)
-    assert len(paths) == 1
+    paths = browser.take_screenshot(url, 100, 5000)
+    assert len(paths) == length
     output_path = "movie/test.mp4"
     image_to_movie(paths, output_path)
     assert os.path.exists(output_path) is True
