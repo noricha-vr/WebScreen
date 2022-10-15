149bb66dd0fb6062dc71c7b65ccc7e921f1850b8
noricha-vr <noricha.vr@gmail.com>
2022-10-16 02:06:14 +0900

    remove unused methods.

--- gcs.py
+++ gcs.py
@@ -5,10 +5,8 @@ class BucketManager:
     """Uploads a file to the bucket.
     This class has 4 methods:
     1. upload_file: Uploads a file to the bucket. return None
-    2. delete_file: Deletes a file from the bucket. return bool
-    3. generate_signed_url: Get a file credential url from the bucket. return str
-    4. public_file_url: Get a file public url from the bucket. return str
-    5. make_public: Make a file public. return bool
+    2. public_file_url: Get a file public url from the bucket. return str
+    3. make_public: Make a file public. return bool
     """
 
     def __init__(self, bucket_name: str):
@@ -25,24 +23,6 @@ class BucketManager:
         """
         blob = self.bucket.blob(file_name)
         blob.upload_from_filename(file_path)
-
-    def delete_file(self, file_name: str) -> bool:
-        """
-        Deletes a file from the bucket.
-        :param file_name:
-        :return bool:
-        """
-        blob = self.bucket.blob(file_name)
-        return blob.delete()
-
-    def generate_signed_url(self, file_name: str) -> str:
-        """
-        Get a file credential url from the bucket.
-        :param file_name:
-        :return str:
-        """
-        blob = self.bucket.get_blob(file_name)
-        return blob.generate_signed_url(expiration=3600)
 
     def get_public_file_url(self, file_name: str) -> str:
         """
