593b11e3391a9af69add75f8957102db3b78ee89
noricha-vr <noricha.vr@gmail.com>
2022-10-19 10:34:31 +0900

    remove github_converter.py

--- file_handler.py
+++ file_handler.py
@@ -34,24 +34,3 @@ class FileHandler:
         :return: file path
         """
         return Path(f"image/{Path(file_name).stem}/movie.mp4")
-
-    @staticmethod
-    def unzip_file(zip_file_path: Path, folder_path: Path) -> None:
-        """
-        Unzip file
-        :param zip_file_path:
-        :param folder_path:
-        """
-        with zipfile.ZipFile(zip_file_path) as existing_zip:
-            existing_zip.extractall(folder_path)
-
-    @staticmethod
-    def download_file(url: str, file_path: Path) -> None:
-        """
-        Download git archive
-        :param url:
-        :param file_path:
-        """
-        response = requests.get(url)
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
