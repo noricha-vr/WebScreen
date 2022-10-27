cc3c73ec5c5efd8a37aab90777f4e80199e3c25a
noricha-vr <noricha.vr@gmail.com>
2022-10-27 13:51:10 +0900

    add example.

--- shell/post_mac_desktop.sh
+++ shell/post_mac_desktop.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # 1. Take a screenshot of your desktop
-# 2. Post the image to the following URL "http://0.0.0.0:8080/post_desktop/". post with session_id and image
+# 2. Post the image to the following URL "http://0.0.0.0:8080/api/desktop/". post with session_id and image
 # 3. Execute 1 - 2 every second
 
+# local test
 while true;
 do
-    # Take a screenshot of your desktop
     screencapture -x -t jpg /tmp/desktop.jpg
-    # Post the image to the following URL "http://0.0.0.0:8080/post_desktop/"
-    curl -X POST -F "file=@/tmp/desktop.jpg" -H 'session_id: xxx' http://0.0.0.0:8080/desktop-image/
-    sleep 1
+    curl -X POST -F "file=@/tmp/desktop.jpg" -H 'X-Token: xxx' http://0.0.0.0:8080/api/desktop/
+    sleep 3
 done
+
+# example
+#do
+#    screencapture -x -t jpg /tmp/desktop.jpg
+#    curl -X POST -F "file=@/tmp/desktop.jpg" -H 'X-Token: xxx' https://screen-capture-7fhttuy7sq-an.a.run.app/api/desktop/
+#    sleep 3
+#done
