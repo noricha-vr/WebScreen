f0cd5d5e28b3d18e8d8dd62008785068c7d117c9
noricha-vr <noricha.vr@gmail.com>
2022-11-02 00:50:54 +0900

    use async await

--- static/js/request.js
+++ static/js/request.js
@@ -1,26 +1,26 @@
 const CREATE_MOVIE_URL = '/api/create_movie/';
 
-function createMovie(movie) {
+const submitButton = document.getElementById('submit');
+const loadingImage = document.getElementById('loading');
+const movieUrlElement = document.getElementById('movie_url');
+const copyButton = document.getElementById('copy');
+
+async function createMovie(movie) {
     let request_url = `${CREATE_MOVIE_URL}?url=${movie.url}&max_page_height=${movie.max_page_height}` +
         `&width=${movie.width}&height=${movie.height}`;
     console.log(`Requesting ${request_url}`);
-    return fetch(request_url, {
+    return await fetch(request_url, {
         method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        },
     });
 }
 
 
 // when click submit button, create movie
-document.getElementById('submit').addEventListener('click', async () => {
-    console.log('submit');
+submitButton.addEventListener('click', async () => {
     // invisible submit button. add  visually-hidden class
-    document.getElementById('submit').className = 'visually-hidden';
+    submitButton.className = 'visually-hidden';
     // visible loading button. remove visually-hidden class
-    document.getElementById('loading').className = '';
+    loadingImage.className = '';
     let movie = {
         url: encodeURI(document.getElementById('url').value),
         max_page_height: document.getElementById('max_page_height').value,
@@ -28,37 +28,34 @@ document.getElementById('submit').addEventListener('click', async () => {
         height: document.getElementById('height').value,
     }
     console.log(`movie: ${JSON.stringify(movie)}`);
-    createMovie(movie).then((response) => {
-        console.log(`response: ${JSON.stringify(response)}`);
-        // show submit button
-        document.getElementById('submit').className = 'btn btn-primary';
-        // hide loading button
-        document.getElementById('loading').className = 'visually-hidden';
-        if (response.status === 200) {
-            // set url to movie_url
-            response.json().then((data) => {
-                console.log(`data: ${JSON.stringify(data)}`);
-                // set movie_url
-                document.getElementById('movie_url').href = data.url;
-                document.getElementById('movie_url').innerHTML = data.url;
-                // show movie_url
-                document.getElementById('result').className = '';
-            });
-        } else {
-            alert('error');
-        }
-    });
+    let response = await createMovie(movie)
+    // show submit button
+    submitButton.className = 'btn btn-primary';
+    // hide loading button
+    loadingImage.className = 'visually-hidden';
+    if (response.status === 200) {
+        // set url to movie_url
+        let data = await response.json()
+        console.log(`data: ${JSON.stringify(data)}`);
+        // set movie_url
+        movieUrlElement.href = data.url;
+        movieUrlElement.innerHTML = data.url;
+        // show movie_url
+        document.getElementById('result').className = '';
+    } else {
+        alert('error');
+    }
 });
 
 function copyToClipboard() {
-    let copyText = document.getElementById("movie_url").href;
+    let copyText = movieUrlElement.href;
     navigator.clipboard.writeText(copyText).then(r => {
         console.log('copied');
-        document.getElementById('copy').innerHTML = 'Copied!';
+        copyButton.innerHTML = 'Copied!';
     });
 }
 
 // when click copy button, copy movie_url to clipboard
-document.getElementById('copy').addEventListener('click', () => {
+copyButton.addEventListener('click', () => {
     copyToClipboard();
 })
