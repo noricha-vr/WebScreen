310851ffaad4ef235bdcc2f32578a744704b88f1
noricha-vr <noricha.vr@gmail.com>
2022-11-06 23:49:24 +0900

    change request.js functions

--- static/js/request.js
+++ static/js/request.js
@@ -6,7 +6,16 @@ const movieUrlElement = document.getElementById('movie_url');
 const copyButton = document.getElementById('copy');
 const resultArea = document.getElementById('result');
 
-async function createMovie(settings) {
+// when click submit button, create movie
+submitButton.addEventListener('click', submit);
+
+async function fetchMovieUrl() {
+    let settings = {
+        url: encodeURI(document.getElementById('url').value),
+        max_page_height: document.getElementById('max_page_height').value,
+        width: document.getElementById('width').value,
+        height: document.getElementById('height').value,
+    }
     let request_url = `${CREATE_MOVIE_URL}?url=${settings.url}&max_page_height=${settings.max_page_height}` +
         `&width=${settings.width}&height=${settings.height}`;
     console.log(`Requesting ${request_url}`);
@@ -16,22 +25,14 @@ async function createMovie(settings) {
 }
 
 
-// when click submit button, create movie
-submitButton.addEventListener('click', async () => {
+async function submit() {
     // Hide submit button and result area.
     submitButton.className = 'visually-hidden';
     resultArea.className = 'visually-hidden';
     // show loading button. remove visually-hidden class
     loadingImage.className = '';
-
-    let settings = {
-        url: encodeURI(document.getElementById('url').value),
-        max_page_height: document.getElementById('max_page_height').value,
-        width: document.getElementById('width').value,
-        height: document.getElementById('height').value,
-    }
-    console.log(`movie: ${JSON.stringify(settings)}`);
-    let response = await createMovie(settings)
+    // fetch movie url
+    let response = await fetchMovieUrl()
     // show submit button
     submitButton.className = 'btn btn-primary';
     // hide loading button
@@ -49,7 +50,8 @@ submitButton.addEventListener('click', async () => {
     } else {
         alert('error');
     }
-});
+}
+
 
 function copyToClipboard() {
     let copyText = movieUrlElement.href;
