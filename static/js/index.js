b60f007c163ff064e7e6ee15a4e693ef77c3ea82
noricha-vr <noricha.vr@gmail.com>
2022-11-14 23:24:48 +0900

    add invalid url error message.

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,13 @@
 
 async function fetchMovieUrl() {
     let url = encodeURI(document.getElementById('url').value)
+    if (!url.startsWith('http')) {
+        return new Response('Invalid URL', {status: 400});
+    }
     let widthHeight = document.querySelector('input[name="width_height"]:checked').value.split('x');
+    let cache = document.getElementById('catch').checked;
     let request_url = `/api/create_movie/?url=${url}&page_height=${pageHeightSlider.value}` +
-        `&width=${widthHeight[0]}&height=${widthHeight[1]}&lang=${navigator.language}`;
+        `&width=${widthHeight[0]}&height=${widthHeight[1]}&lang=${navigator.language}&catch=${cache}`;
     console.log(`Requesting ${request_url}`);
     return await fetch(request_url, {
         method: 'GET',
