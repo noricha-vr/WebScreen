225705a7f9aba878a61b36bcbbcaba5ed5f062c5
noricha-vr <noricha.vr@gmail.com>
2022-12-04 22:54:46 +0900

    Move uuid to baseFunctions.js

--- static/js/baseFunctions.js
+++ static/js/baseFunctions.js
@@ -109,6 +109,13 @@ function showResultMessage() {
     }
 }
 
+
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 window.onload = () => {
     selectActiveMenu();
     addResultsToPage();
--- static/js/desktopCapture.js
+++ static/js/desktopCapture.js
@@ -101,12 +101,6 @@ function dumpOptionsInfo() {
 }
 
 
-function uuidv4() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-}
-
 window.onload = function () {
     let uuid = localStorage.getItem("uuid");
     if (uuid === null) {
