3fa96aa878075706a9f67e5e3b5c9cccf46bd679
noricha-vr <noricha.vr@gmail.com>
2022-12-20 14:28:52 +0900

    set -hls_time 3

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ def to_m3u8(input_path: Path, output_path: Path, base_url: str, buffer_sec=3):
     command = f'ffmpeg -re -i {input_path} ' \
               f'-c:v copy ' \
               f'-c:a aac -b:a 128k -strict -2 ' \
-              f'-f hls -hls_time 1 ' \
+              f'-f hls -hls_time 3 ' \
               f"-hls_flags delete_segments " \
               f'-hls_segment_filename "{output_path.parent}/video%3d.ts" ' \
               f'-hls_base_url {base_url} ' \
