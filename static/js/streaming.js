35b5d7d5b934e33f1d3f6a8abfd7ea5339dca59e
noricha-vr <noricha.vr@gmail.com>
2022-12-25 19:17:07 +0900

    Move: uuidv4 on top.

--- static/js/streaming.js
+++ static/js/streaming.js
@@ -97,13 +97,6 @@ async function startRecording() {
     setTimeout(stopRecording, 1000 * 60 * 30)
 }
 
-function uuidv4() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-}
-
-
 function setupCountdown() {
     /*
     * Set up a countdown timer to start recording after 15 seconds.
