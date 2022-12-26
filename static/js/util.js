ac53f23df46f3b7ee539237b3d9d44068f3b0603
noricha-vr <noricha.vr@gmail.com>
2022-12-27 00:21:58 +0900

    Add util.js

---
+++ static/js/util.js
@@ -0,0 +1,40 @@
+'use static';
+
+function selectAllText(e) {
+    e.target.setSelectionRange(0, e.target.value.length);
+}
+
+
+function pasteFromClipboard(targetTextElement) {
+    navigator.clipboard.readText().then(text => {
+        targetTextElement.value = text;
+    });
+}
+
+
+function saveResult(result) {
+    /*
+    Save result in cookie.
+     */
+    if (result.delete_at === null) {
+        console.log(`Saving result in cookie: ${result.url}`);
+        return;
+    }
+    let expires = new Date(result.delete_at * 1000);
+    let cookie = `${result.name}=${result.url}; expires=${expires.toUTCString()}; path=/`;
+    console.log(`Saving result in cookie: ${cookie}`);
+    document.cookie = cookie;
+}
+
+function showBrowserErrorMessage() {
+    // Regex: Google Chrome or Microsoft Edge
+    let supportedBrowsersRegex = /Chrome|Edge/i;
+
+    // Check Google Chrome or Microsoft Edge
+    let isSupportedBrowser = supportedBrowsersRegex.test(navigator.userAgent);
+
+    // If current browser is not Google Chrome or Microsoft Edge, Show #browser-error-message
+    if (!isSupportedBrowser) {
+        document.getElementById('browser-error-message').classList.remove('visually-hidden');
+    }
+}
