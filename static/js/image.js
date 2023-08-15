a12fca43b94ecbd9235f84e8a239d23e3c6d0ef1
noricha-vr <noricha.vr@gmail.com>
2023-08-15 06:54:23 +0000

    show file size error message.

--- static/js/image.js
+++ static/js/image.js
@@ -1,18 +1,36 @@
-'use static';
+'use strict';
 
 async function fetchMovieUrl() {
     let request_url = `/api/image-to-movie/`;
-    console.log(`Requesting ${request_url}`);
+    const fileInput = document.getElementById('files');
+    const files = fileInput.files;
+    totalFileSizeValidation(files, 'The total file size is too large. It should be smaller than 32MB.');
+
     let formData = new FormData();
-    let files = document.getElementById('files').files;
     for (let i = 0; i < files.length; i++) {
         formData.append('images', files[i]);
     }
+
     return await fetch(request_url, {
         method: 'POST',
         body: formData
     });
 }
+
+function totalFileSizeValidation(files, message) {
+    const maxFileSize = 32 * 1024 * 1024; // 32MB in bytes
+    let totalSize = 0;
+
+    for (let i = 0; i < files.length; i++) {
+        totalSize += files[i].size;
+    }
+
+    if (totalSize > maxFileSize) {
+        alert(message);
+        window.location.reload();
+    }
+}
+
 
 function addEventListeners() {
     submitButton.addEventListener('click', submit);
--- static/js/pdf.js
+++ static/js/pdf.js
@@ -1,17 +1,32 @@
 'use static';
 
 async function fetchMovieUrl() {
+    const fileInput = document.getElementById('files');
+    const file = fileInput.files?.[0];
+    fileSizeValidation(file, 'This file is too large. File size should be smaller than 32MB.')
+
     let request_url = `/api/pdf-to-movie/`;
     console.log(`Requesting ${request_url}`);
-    let frame_sec = parseInt(document.querySelector('input[name="frame_sec"]:checked').value);
+    let frame_sec = parseInt((document.querySelector('input[name="frame_sec"]:checked')).value);
     let formData = new FormData();
-    formData.append('pdf', document.getElementById('files').files[0]);
-    formData.append('frame_sec', frame_sec);
+    formData.append('pdf', file);
+    formData.append('frame_sec', frame_sec.toString());
+
     return await fetch(request_url, {
         method: 'POST',
         body: formData
     });
 }
+
+
+function fileSizeValidation(file, message) {
+    const maxFileSize = 32 * 1024 * 1024; // 32MB in bytes
+    if (file && file.size > maxFileSize) {
+        alert(message);
+        window.location.reload()
+    }
+}
+
 
 function addEventListeners() {
     submitButton.addEventListener('click', submit);
